d91fe74b69650e4705aab3b58406392c146df5a4
Akshay Kumar Shaw <akshaykumarshaw44@gmail.com>
2026-03-19 10:44:59 +0530

    adding auth middleware

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,11 @@
+export const protect = async (req,res,next) => {
+    try {
+        const { userId } = await req.auth()
+        if(!user){
+            return res.json({success: false, message: "Not Authenticated"})
+        }
+        next()
+    } catch (error) {
+        res.json({success: false, message: "Not Authenticated"})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 import { inngest, functions } from './inngest/index.js';
 import {serve} from 'inngest/express'
+import { clerkMiddleware } from '@clerk/express'
+
 
 const PORT = process.env.PORT || 4000
 
@@ -15,6 +17,7 @@ connectDB()
 
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get('/',(req,res)=>res.send('Server is running'))
 app.use('/api/inngest',serve({client: inngest, functions: functions}))
