3902657ac51c519bddd3462488683f4c5014606a
Akshay Kumar Shaw <akshaykumarshaw44@gmail.com>
2026-03-21 04:23:08 +0530

    app.use(cors({   origin: true,   credentials: true }));

--- server/server.js
+++ server/server.js
@@ -14,10 +14,8 @@ connectDB()
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: true,
+  credentials: true
 }));
 app.use(clerkMiddleware());
 
