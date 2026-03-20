aa8ad28b1912dfa680bf9da8f1c0c86893674e89
Akshay Kumar Shaw <akshaykumarshaw44@gmail.com>
2026-03-21 00:50:21 +0530

    added router file

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { connectDB } from "./configs/db.js";
 import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
+import userRouter from "./routes/user.route.js";
 
 
 const app = express();
@@ -24,6 +25,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/inngest", serve({client: inngest, functions}));
+app.use('/api/user',userRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
