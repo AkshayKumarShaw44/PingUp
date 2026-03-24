316129367cf2bab0ff2820f2cb76c8d723a6d2de
Akshay Kumar Shaw <akshaykumarshaw44@gmail.com>
2026-03-24 22:18:29 +0530

    check         return res.status(200).json({success: true, data: user})

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -15,7 +15,7 @@ export const getUserData = async (req, res) => {
         if(!user){
             return res.status(404).json({success: false, message: "User not found"})
         }
-        return res.status(200).json({success: true, data: user})
+        return res.status(200).json({success: true, user})
     } catch (error) {
         return res.status(500).json({success: false, message: error.message})
     }
