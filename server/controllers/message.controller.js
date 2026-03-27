142bb29101eb9823b297f4c23db39cd963d14b50
Akshay Kumar Shaw <akshaykumarshaw44@gmail.com>
2026-03-28 03:28:23 +0530

    added recent message feature PROJECT COMPLETED

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -99,7 +99,7 @@ export const getChatMessages = async (req, res) => {
 export const getUserReceentMessages = async (req,res) => {
     try {
         const { userId } = await req.auth()
-        const messages = await Message.find({to_user_id: userId}.populate('from_user_id to_user_id')).sort({created_at : -1})
+        const messages = await Message.find({to_user_id: userId}).populate('from_user_id to_user_id').sort({created_at : -1})
         res.json({success: true, messages})
     } catch (error) {
         res.json({success: false, message: error.message})
